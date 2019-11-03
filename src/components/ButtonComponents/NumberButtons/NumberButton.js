585fea168537d89e57924a112aebebcb279d7dbd
Code4Blessings <robinwarden@code4blessings.com>
2019-11-03 14:24:43 -0500

    Created Number.css file and styled number buttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -7,7 +7,7 @@ const NumberButton = (props) => {
   return (
     <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-        <button onClick={number}>{buttonValue}</button>  
+        <button className="number-btn" onClick={number}>{buttonValue}</button>  
     </div>
   );
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { numbers } from "../../../data";
 import NumberButton from "./NumberButton";
+import "./Number.css";
 
 //import any components needed X
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -25,26 +26,31 @@ const Numbers = () => {
  const display7 = () => setNumberState(7);
  const display8 = () => setNumberState(8);
  const display9 = () => setNumberState(9);
- const display10 = () => setNumberState(0);
- 
+ const display0 = () => setNumberState(0);
  
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-       <h2>The Number is: {numberState}</h2>
-      <NumberButton number={display1} buttonValue={numbers[0]} />
-      <NumberButton number={display2}buttonValue={numbers[1]} />
-      <NumberButton number={display3} buttonValue={numbers[2]} />
-      <NumberButton number={display4} buttonValue={numbers[3]} />
-      <NumberButton number={display5} buttonValue={numbers[4]} />
-      <NumberButton number={display6} buttonValue={numbers[5]} />
-      <NumberButton number={display7} buttonValue={numbers[6]} />
-      <NumberButton number={display8} buttonValue={numbers[7]} />
-      <NumberButton number={display9} buttonValue={numbers[8]} />
-      <NumberButton number={display10} buttonValue={numbers[9]} />
-      <NumberButton buttonValue={numbers[10]} /> 
-    
+    <div className="title">
+      <div className="number-title">
+          <h2>The Number is: {numberState}</h2>
+      </div>
+
+      <div className="btn-container">
+        {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass
+        it any props needed by the child component*/}
+        
+        <NumberButton number={display1} buttonValue={numbers[0]} />
+        <NumberButton number={display2}buttonValue={numbers[1]} />
+        <NumberButton number={display3} buttonValue={numbers[2]} />
+        <NumberButton number={display4} buttonValue={numbers[3]} />
+        <NumberButton number={display5} buttonValue={numbers[4]} />
+        <NumberButton number={display6} buttonValue={numbers[5]} />
+        <NumberButton number={display7} buttonValue={numbers[6]} />
+        <NumberButton number={display8} buttonValue={numbers[7]} />
+        <NumberButton number={display9} buttonValue={numbers[8]} />
+        <NumberButton number={display0} buttonValue={numbers[9]} />
+        <NumberButton buttonValue={numbers[10]} /> 
+      
+      </div>
     </div>
   );
 };
