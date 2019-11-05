6118c8929e08ddec9c0a5c3da322653e3e720443
Code4Blessings <robinwarden@code4blessings.com>
2019-11-05 15:56:21 -0500

    Added useState hook to each of the button files

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { operators } from "../../../data";
 import OperatorButton from "./OperatorButton";
 import "./Operator.css";
@@ -9,13 +9,13 @@ import "./Operator.css";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  
+  const [operatorState] = useState(operators);
   return (
     <div className="operator-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {operators.map((button, index) => (
+       {operatorState.map((button, index) => (
           <OperatorButton key={index} button={button} />
         ))}
     </div>
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { specials } from "../../../data";
 import SpecialButton from "./SpecialButton";
 import "./Specials.css";
@@ -9,13 +9,14 @@ import "./Specials.css";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState] = useState(specials);
 
   return (
     <div className="specials-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {specials.map((index) => (
+       {specialState.map((index) => (
           <SpecialButton buttonValue={index} />
         ))}
     </div>
